533bf8cde506b10825befa15c347242c295c56b4
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2026-04-15 11:40:21 -0500

    Add responsive mobile navigation with Angular Material. Updated `navbar` component to include hamburger menu for smaller screens and adjusted CSS for seamless desktop and mobile experience.

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+// Importamos el menú y los íconos
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
